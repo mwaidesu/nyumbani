8653e5c11894a4f16a3ec3fd74490e77ce82b5e7
Irene Njeru <irenenjeru8@gmail.com>
2022-08-19 23:43:31 +0300

    add Destination component details

--- src/components/quote/Destination.js
+++ src/components/quote/Destination.js
@@ -5,6 +5,22 @@ export default function Destination() {
   return (
     <div className="quote">
       <h3>Destination Confirmation</h3>
+      <div className="row">
+          <p className="col">Origin</p>
+          <p className="col">Kahawa Sukari</p>
+      </div>
+      <div className="row">
+          <p className="col">Destination</p>
+          <p className="col">Kilimani</p>
+      </div>
+      <div className="row">
+          <p className="col">Moving Company</p>
+          <p className="col">Movers N Shakers</p>
+      </div>
+      <div className="row">
+          <p className="col">Kilometers</p>
+          <p className="col">14</p>
+      </div>
       <Form>
         <div className="center">
           <Button>Next</Button>
