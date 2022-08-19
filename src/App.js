82d6af533e6cac14cca8000fe2b768d41099c756
Mwai <machariamwai@gmail.com>
2022-08-19 16:56:12 +0300

    homepage first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
-import './App.css';
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
+import { Navbar } from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-     Trial
+      <Navbar />
+      <Home />
+      <Footer/>
     </div>
   );
 }
