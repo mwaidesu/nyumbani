d4a51a59fbaa17421bfa37d4e802ff5e109e47df
Trizah <wangarijoytriza@gmail.com>
2022-08-22 12:42:54 +0300

    Testimonials

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Testimonials from './components/Cards/Testimonials';
+import Company from './components/Company';
 
 function App() {
   return (
     <div className="App">
-     Trial
+      <Testimonials />
+      <Company />
     </div>
   );
 }
