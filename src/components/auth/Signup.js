a1ca357b1e1e52c65d73955d5279cfc6d133c1de
Irene Njeru <irenenjeru8@gmail.com>
2022-08-23 11:12:24 +0300

    Add style to signup and Sign In

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -62,7 +62,7 @@ function SignUp() {
         </div>
       )}
       <form onSubmit={handleSubmit}>
-        <h2 className="text-4xl my-5">Sign Up</h2>
+        <h2 className="text-4xl my-5 mx-auto">Sign Up</h2>
         <div class="mb-6">
           <label
             for="name"
@@ -147,7 +147,7 @@ function SignUp() {
         <button
           disabled={loading}
           type="submit"
-          class="text-white bg-amber-700 hover:bg-amber-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-amber-600 dark:hover:bg-amber-700 dark:focus:ring-amber-800"
+          class="text-white bg-amber-500 hover:bg-amber-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-amber-600 dark:hover:bg-amber-700 dark:focus:ring-amber-800"
         >
           Sign Up
         </button>
