7c497f59bcac5c34e40c366d05905f7b3fc6db12
Trizah <wangarijoytriza@gmail.com>
2022-08-24 15:38:53 +0300

    Add company

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Hero from './components/Home/Hero'
 import './App.css';
 import Testimonials from './components/Cards/Testimonials';
+import Company from './components/Company';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 function App() {
@@ -8,6 +11,7 @@ function App() {
     <div className="App">
       <Hero/>
       <Testimonials/>
+      <Company />
     </div>
   );
 }
