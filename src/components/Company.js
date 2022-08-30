f5d906eff3e877c69aafc09f7ff97513a54e49d2
Trizah <wangarijoytriza@gmail.com>
2022-08-30 14:52:05 +0300

    Add content

--- src/components/Company.js
+++ src/components/Company.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Company = () => {
   return (
-
-    <div className="grid grid-cols-10 md:grid-cols-12 my-2">
+    
+    <div className="grid grid-cols-10 md:grid-cols-12 my-2 bg-black">
       <div className='hidden md:contents'><div></div></div>
       <div className="col-span-10">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mx-1">
@@ -11,14 +11,14 @@ const Company = () => {
             <div>
               <img
                 className="rounded-t-lg"
-                src="https://us.123rf.com/450wm/karelnoppe/karelnoppe1601/karelnoppe160100010/51235375-close-up-outdoor-portrait-of-attractive-african-teen-girl-with-braided-hairstyle-.jpg?ver=6"
+                src="https://packitmoversfranchising.com/wp-content/uploads/2021/04/how-to-start-a-moving-company-3.jpg"
                 alt=""
               />
             </div>
             <div className="p-1">
               <div>
                 <h5 className="text-base font-bold tracking-tight text-gray-900 dark:text-white">
-                  Amani Cherera
+                Movers and shakers
                 </h5>
               </div>
               <p className="text-xs text-gray-700 dark:text-gray-400">
@@ -31,14 +31,14 @@ const Company = () => {
             <div>
               <img
                 className="rounded-t-lg"
-                src="https://us.123rf.com/450wm/karelnoppe/karelnoppe1601/karelnoppe160100010/51235375-close-up-outdoor-portrait-of-attractive-african-teen-girl-with-braided-hairstyle-.jpg?ver=6"
+                src="https://packitmoversfranchising.com/wp-content/uploads/2021/04/how-to-start-a-moving-company-3.jpg"
                 alt=""
               />
             </div>
             <div className="p-1">
               <div>
                 <h5 className="text-base font-bold tracking-tight text-gray-900 dark:text-white">
-                  Amani Cherera
+                   Utahama Lini
                 </h5>
               </div>
               <p className="text-xs text-gray-700 dark:text-gray-400">
@@ -51,14 +51,14 @@ const Company = () => {
             <div>
               <img
                 className="rounded-t-lg"
-                src="https://us.123rf.com/450wm/karelnoppe/karelnoppe1601/karelnoppe160100010/51235375-close-up-outdoor-portrait-of-attractive-african-teen-girl-with-braided-hairstyle-.jpg?ver=6"
+                src="https://packitmoversfranchising.com/wp-content/uploads/2021/04/how-to-start-a-moving-company-3.jpg"
                 alt=""
               />
             </div>
             <div className="p-1">
               <div>
                 <h5 className="text-base font-bold tracking-tight text-gray-900 dark:text-white">
-                  Amani Cherera
+                  Nyumbani Move
                 </h5>
               </div>
               <p className="text-xs text-gray-700 dark:text-gray-400">
@@ -71,14 +71,14 @@ const Company = () => {
             <div>
               <img
                 className="rounded-t-lg"
-                src="https://us.123rf.com/450wm/karelnoppe/karelnoppe1601/karelnoppe160100010/51235375-close-up-outdoor-portrait-of-attractive-african-teen-girl-with-braided-hairstyle-.jpg?ver=6"
+                src="https://packitmoversfranchising.com/wp-content/uploads/2021/04/how-to-start-a-moving-company-3.jpg"
                 alt=""
               />
             </div>
             <div className="p-1">
               <div>
                 <h5 className="text-base font-bold tracking-tight text-gray-900 dark:text-white">
-                  Amani Cherera
+                  Ofisini Move
                 </h5>
               </div>
               <p className="text-xs text-gray-700 dark:text-gray-400">
