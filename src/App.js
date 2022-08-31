37e10c6c3b6e2f9fcf9061d4adbf8ae3d2785184
Trizah <wangarijoytriza@gmail.com>
2022-08-31 10:59:30 +0300

    Trial to merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import Hero from './components/Home/Hero'
-import './App.css';
-import Testimonials from './components/Cards/Testimonials';
-import Company from './components/Company';
+// import Hero from './components/Home/Hero'
+// import './App.css';
+// import Testimonials from './components/Cards/Testimonials';
+// import Company from './components/Company';
 
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <Hero/>
-      <br/>
-      <Testimonials/>
-      <br/>
-      <Company />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Hero/>
+//       <br/>
+//       <Testimonials/>
+//       <br/>
+//       <Company />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
